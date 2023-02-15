b8588fe6f4e04ca51ea59050d0167c0e84920f2e
yamayuz <yuki@yamayulab.com>
2023-02-15 14:57:12 +0900

    [add] abc-085-b-kagami-mochi

---
+++ practice/abc-085-b-kagami-mochi.py
@@ -0,0 +1,10 @@
+# ABC 085 B - Kagami Mochi 
+# https://atcoder.jp/contests/abc085/tasks/abc085_b
+# 
+
+
+if __name__ == "__main__":
+    n = int(input())
+    d = [int(input()) for _ in range(n)]
+
+    print(len(set(d)))
